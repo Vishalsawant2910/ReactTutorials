6eff3e4cc81fa3390ea2551d441628d72fcdb537
Vishalsawant2910 <vishalsawant2910@gmail.com>
2024-08-10 14:59:13 +0530

    React demo_2

---
+++ src/App1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App1() {
+  return (
+    <div className="App">
+     <h1>Hello React from App1</h1>
+    </div>
+  );
+}
+
+export default App1;
